52de0b5c36811020a329d635d3f3a7bff0ae51d5
Di Zhang <zhangdi_me@163.com>
2018-01-23 13:27:06 +0800

    Update

--- FontAwesomeAsset.php
+++ FontAwesomeAsset.php
@@ -14,4 +14,13 @@ class FontAwesomeAsset extends AssetBundle
     public $css = [
         'css/font-awesome.css'
     ];
+
+    public function init()
+    {
+        parent::init();
+
+        $this->publishOptions['beforeCopy'] = function ($from, $to) {
+            return preg_match('%(/|\\\\)(fonts|css)%', $from);
+        };
+    }
 }
